46e28779a9054f4740b7819ca5654b9c3ca424da
feng <sanctitylight12@gmail.com>
2025-04-25 16:51:31 +0800

    創建項目

---
+++ study-project-backend/src/test/java/org/example/studyprojectbackend/StudyProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.studyprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
